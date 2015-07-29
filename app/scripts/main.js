27ff4331461ee258966333ab3a76a8280c711b48
Ian Jones <hello@ianjones.id.au>
2015-07-29 16:53:33 +1000

    Change problem text example

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,7 +12,7 @@ $('#masthead .call-to-action').on('click', function(event) {
 
 var theater = new TheaterJS();
 theater.describe("Problem", { speed: 1, accuracy: 1}, "#typer");
-theater .write( "Problem:Deliver a memorable wedding speech", 3000)
+theater .write("Problem:Captivate wedding guests", 3000)
         .write("Problem:Inspire your new team at work", 3000)
         .write("Problem:Nail that job interview", 3000)
         .write("Problem:Build networking confidence", 3000)
