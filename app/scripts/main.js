a2edeaf13c0f7869fe67aba2488ac7461349423d
Ian Jones <hello@ianjones.id.au>
2015-07-29 15:49:10 +1000

    Change problem text, change problem line height

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,11 +12,10 @@ $('#masthead .call-to-action').on('click', function(event) {
 
 var theater = new TheaterJS();
 theater.describe("Problem", { speed: 1, accuracy: 1}, "#typer");
-theater //.write( "Problem:Deliver a memorable wedding speech", 3000)
+theater .write( "Problem:Deliver a memorable wedding speech", 3000)
         .write("Problem:Inspire your new team at work", 3000)
         .write("Problem:Nail that job interview", 3000)
-        .write("Problem:Improve networking abilities", 3000)
-        .write("Problem:No longer be the quiet one", 3000)
+        .write("Problem:Build networking confidence", 3000)
         .write(function () { theater.play(true); });
 
 
